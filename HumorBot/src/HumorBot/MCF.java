d3fc1e88f491b2f54f584621008528d5cfd44e7b
Peter Xeros <pxeros@purdue.edu>
2018-02-10 16:45:52 -0500

    Added a Black Card class and one variable to MCF.java

--- HumorBot/src/HumorBot/MCF.java
+++ HumorBot/src/HumorBot/MCF.java
@@ -1,5 +1,10 @@
 package HumorBot;
-
+//Imports
+import HumorBotGUI.*;
+import java.util.*;
+//Here is the class
 public class MCF {
-
+	//Here is our hand
+	private ArrayList<WhiteCard> hand = new ArrayList<WhiteCard>();
+	//
 }
