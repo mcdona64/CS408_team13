266b8c2169bbd60877b6905408c865353cbf3398
Peter Xeros <pxeros@purdue.edu>
2018-02-10 18:11:55 -0500

    Started MCF, added BlackCard, and modified white card to make more sense

--- HumorBot/src/HumorBot/MCF.java
+++ HumorBot/src/HumorBot/MCF.java
@@ -2,14 +2,33 @@ package HumorBot;
 //Imports
 import HumorBotGUI.*;
 import java.util.*;
-//Here is the class
+/**
+ * @author pxeros
+ * This class is the main control flow for our AI
+ */
 public class MCF {
 	//Here is our hand
 	private ArrayList<WhiteCard> hand = new ArrayList<WhiteCard>();
-	//
-	public static void main(String[] args) {
-		Web w = new Web("http://www.pretendyoure.xyz/zy/");
-		w.grabWebpage();
+	//This is the username
+	private String username;
+	//This is the web interface we will be using
+	private Web crawler;
+	//This is the number of point we have (or black Cards we have won)
+	private int points;
+	//This is the current blackCard on the table
+	private BlackCard currentCard;
+	/**
+	 * This is the constructor 
+	 */
+	public MCF(){}
+	
+	public void getHand(){
+		
+	}
+	
+	private void initializeWebCrawler(){
+		this.crawler = new Web("http://www.pretendyoure.xyz/zy/");
+		this.crawler.grabWebpage();
 	}
 	
 }
