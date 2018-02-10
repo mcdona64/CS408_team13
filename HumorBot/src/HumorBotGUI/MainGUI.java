7b6c52b7dbb577684848ff18795db05105aaa941
Avi Agarwal <agarwa68@pod3-3.cs.purdue.edu>
2018-02-10 18:34:54 -0500

    Main Menu Java class

--- HumorBot/src/HumorBotGUI/MainGUI.java
+++ HumorBot/src/HumorBotGUI/MainGUI.java
@@ -1,5 +1,53 @@
 package HumorBotGUI;
 
-public class MainGUI {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class MainGUI {
+    private JButton offlineMode;
+    private JButton options;
+    private JButton onlineMode;
+    private JButton spectatorMode;
+    private JPanel panelMenu;
+
+    public MainGUI() {
+        offlineMode.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "OFFLINE MODE Has been Launched");
+            }
+        });
+        onlineMode.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "ONLINE MODE Has been Launched");
+            }
+        });
+        spectatorMode.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "SPECTATOR MODE Has been Launched");
+            }
+        });
+        options.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "OPTIONS MENU Has been Launched");
+            }
+        });
+    }
+
+    public void launchStartMenu() {
+        JFrame frame = new JFrame("MainMenu");
+        frame.setContentPane(new MainGUI().panelMenu);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        MainGUI m = new MainGUI();
+        m.launchStartMenu();
+    }
 }
