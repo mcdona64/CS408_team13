e7373dc9a3cf2bca1eb31d432161a9a78da992b5
mcdona64 <mcdona64@purdue.edu>
2018-02-10 22:36:55 -0500

    added class for talking to the DB(not Implemented)

---
+++ HumorBot/src/HumorBot/DatabaseInterface.java
@@ -0,0 +1,69 @@
+package HumorBot;
+
+public class DatabaseInterface {
+
+    public WhiteCard getWhiteCard(String name){
+        // TODO
+        return null;
+    }
+
+    public WhiteCard getWhiteCard(WhiteCard card){
+        // TODO
+        return null;
+    }
+
+    public BlackCard getBlackCard(String name){
+        // TODO
+        return null;
+    }
+
+    public BlackCard getBlackCard(BlackCard card){
+        // TODO
+        return null;
+    }
+
+    public int addWhiteCard(){
+        // TODO
+        return -1;
+    }
+
+    public int addBlackCard(){
+        // TODO
+        return -1;
+    }
+
+    public int removeWhiteCard() {
+        // TODO
+        return -1;
+    }
+
+    public int removeBlackCard() {
+        // TODO
+        return -1;
+    }
+
+    public int resetDB() {
+        // TODO
+        return -1;
+    }
+
+    public int adjustWeights(WhiteCard winner, BlackCard blackcard){
+        // TODO
+        return -1;
+    }
+
+    public int adjustWeights(String winner, BlackCard blackcard){
+        // TODO
+        return -1;
+    }
+
+    public int adjustWeights(WhiteCard winner, String blackcard){
+        // TODO
+        return -1;
+    }
+
+    public int adjustWeights(String winner, String blackcard){
+        // TODO
+        return -1;
+    }
+}
