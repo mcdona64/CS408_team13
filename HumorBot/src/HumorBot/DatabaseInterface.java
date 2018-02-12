23a9304a1e47ed46496b3dbf8d24de7676d1c3bb
mcdona64 <mcdona64@purdue.edu>
2018-02-11 23:12:50 -0500

    added code to DatabaseInterface

--- HumorBot/src/HumorBot/DatabaseInterface.java
+++ HumorBot/src/HumorBot/DatabaseInterface.java
@@ -1,6 +1,36 @@
 package HumorBot;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseInterface {
+
+    // This variable stores the connection to the database
+    private Connection con;
+
+    // Constructor for the Interface
+    public DatabaseInterface() throws ConnectionNotEstablishedException {
+        System.out.println("establishing connection...");
+        try {
+            this.con = DriverManager.getConnection("128.211.255.58", "root", "password");
+
+        } catch (SQLException e) {
+            System.out.println("connection failed to be established");
+            throw new ConnectionNotEstablishedException();
+        }
+    }
+
+    public void closeConnection(){
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.out.println("closing invalid connection");
+            }
+        }
+        System.out.println("connection closed");
+    }
 
     public WhiteCard getWhiteCard(String name){
         // TODO
