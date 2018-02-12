2b9f24e57e15226df2c58cb40e04cc9811eb2860
mcdona64 <mcdona64@purdue.edu>
2018-02-11 23:31:59 -0500

    added an exception class

---
+++ HumorBot/src/HumorBot/ConnectionNotEstablishedException.java
@@ -0,0 +1,6 @@
+package HumorBot;
+
+/* this is an exception that will be thrown whenever a connection to the database fails to be accepted */
+public class ConnectionNotEstablishedException extends Exception {
+
+}
