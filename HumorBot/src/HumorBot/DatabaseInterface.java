d51040599c399b57163b077cf82b23f7f49f1f92
mcdona64 <mcdona64@purdue.edu>
2018-02-18 15:38:51 -0500

    added ability to add cards to db and test cases

--- HumorBot/src/HumorBot/DatabaseInterface.java
+++ HumorBot/src/HumorBot/DatabaseInterface.java
@@ -1,43 +1,59 @@
 package HumorBot;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatabaseInterface {
 
+    // this variable will hold if the connection is currently established
+    public boolean connected = false;
+
     // This variable stores the connection to the database
-    private Connection con;
+    private Connection conn;
+
+    // This variable stores the sql statment to be sent to the database
+    private Statement stmt;
 
     // Constructor for the Interface
     public DatabaseInterface() throws ConnectionNotEstablishedException {
+        connect();
+    }
+
+    // this function will either connect to the database or throw an error no need for a return value
+    public void connect() throws ConnectionNotEstablishedException {
         System.out.println("establishing connection...");
         try {
             // This will load the MySQL driver, each DB has its own driver
             Class.forName("com.mysql.jdbc.Driver");
             // this will ceate the connection
-            this.con = DriverManager.getConnection("jdbc:mysql://128.211.255.58:3306/humorbot", "sqluser", "sqluserpw");
-
+            this.conn = DriverManager.getConnection("jdbc:mysql://128.211.255.58:3306/humorbot", "sqluser", "sqluserpw");
+            // this will create the statment through which queries are passed
+            stmt = conn.createStatement();
         } catch (SQLException e) {
+            // this exception occurs if the connection can not be established for any reason
+            // we print information to the console for ease of debuging, and then throw a more general exception up
             System.out.println("connection failed to be established");
             System.out.println(e.getMessage());
             throw new ConnectionNotEstablishedException();
         } catch (ClassNotFoundException e) {
+            // This exception happens if the JDBC library can not be found. Check that the project is set up correctly
             System.out.println("failed to load JDBC drivers");
             throw new ConnectionNotEstablishedException();
         }
+        // if no excceptions have been called we are okay and we should tell the user
         System.out.println("connection established");
+        connected = true;
     }
 
     public void closeConnection(){
-        if (con != null) {
+        if (conn != null) {
             try {
-                con.close();
+                conn.close();
             } catch (SQLException e) {
                 System.out.println("closing invalid connection");
             }
         }
         System.out.println("connection closed");
+        connected = false;
     }
 
     public WhiteCard getWhiteCard(String name){
@@ -60,14 +76,46 @@ public class DatabaseInterface {
         return null;
     }
 
-    public int addWhiteCard(){
-        // TODO
-        return -1;
+    public boolean addWhiteCard(String name) throws ConnectionNotEstablishedException {
+        // if we are currently unconnected connect
+        if (!connected){
+            connect();
+        }
+
+        // create the query to add an item to the table
+        String query = "INSERT INTO white_card (name) VALUES ('" + name + "');";
+        try {
+            // executes the query and returns true on success
+            stmt.execute(query);
+            System.out.println("white card \"" + name + "\" added to database");
+            return true;
+        } catch (SQLException e) {
+            // catch errors with the connection
+            System.out.println("Error with connection!");
+            closeConnection();
+            return false;
+        }
     }
 
-    public int addBlackCard(){
-        // TODO
-        return -1;
+    public boolean addBlackCard(String name, int number_of_blanks) throws ConnectionNotEstablishedException {
+        // if we are currently unconnected connect
+        if (!connected){
+            connect();
+        }
+
+        // create the query to add an item to the table
+        String query = "INSERT INTO black_card (name, number_of_blanks) VALUES ('" + name + "', " + number_of_blanks + ");";
+        try {
+            // executes the query and returns true on success
+            stmt.execute(query);
+            System.out.println("black card \"" + name + "\" added to database");
+            return true;
+        } catch (SQLException e) {
+            // catch errors with the connection
+            System.out.println("Error with connection!");
+            closeConnection();
+            return false;
+        }
     }
 
     public int removeWhiteCard() {
--- HumorBot/src/HumorBot/DatabaseInterfaceTest.java
+++ HumorBot/src/HumorBot/DatabaseInterfaceTest.java
@@ -6,10 +6,63 @@ import org.junit.jupiter.api.Test;
 class DatabaseInterfaceTest {
 
     @Test
+    // all other tests will fail if this test fails
     void TestConnection() {
         boolean result = true;
         try {
             DatabaseInterface db = new DatabaseInterface();
+        } catch (ConnectionNotEstablishedException e) {
+            result = false;
+        }
+        assert(result);
+    }
+
+    @Test
+    void TestCloseConnection() {
+        boolean result = true;
+        try {
+            DatabaseInterface db = new DatabaseInterface();
+            db.closeConnection();
+        } catch (ConnectionNotEstablishedException e) {
+            result = false;
+        }
+        assert(result);
+    }
+
+    @Test
+    void TestReConnect() {
+        boolean result = true;
+        try {
+            DatabaseInterface db = new DatabaseInterface();
+            db.closeConnection();
+            db.connect();
+            result = db.connected;
+        } catch (ConnectionNotEstablishedException e) {
+            result = false;
+        }
+        assert(result);
+    }
+
+    @Test
+    // this test may fail if since actual value is not actually checked in this test case
+    void TestAddWhiteCard() {
+        boolean result = true;
+        try {
+            DatabaseInterface db = new DatabaseInterface();
+            result = db.addWhiteCard("Test");
+        } catch (ConnectionNotEstablishedException e) {
+            result = false;
+        }
+        assert(result);
+    }
+
+    @Test
+        // this test may fail if since actual value is not actually checked in this test case
+    void TestAddBlackCard() {
+        boolean result = true;
+        try {
+            DatabaseInterface db = new DatabaseInterface();
+            result = db.addBlackCard("Test", 1);
         } catch (ConnectionNotEstablishedException e) {
             result = false;
         }
