484909212a08fd9753aa7fd9903d6ae0b3973d54
Peter Xeros <pxeros@purdue.edu>
2018-03-09 16:34:52 -0500

    More garbagio

--- HumorBot/src/HumorBotGUI/onlineMode.java
+++ HumorBot/src/HumorBotGUI/onlineMode.java
@@ -4,48 +4,77 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import HumorBot.*;
 
 public class onlineMode {
-
-    public onlineMode()
-    {
-        Frame frame= new JFrame("Online Mode");
+    private Frame frame;
+    private JButton b;
+    public onlineMode() {
+        this.frame = new JFrame("Online Mode");
         //frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        frame.setSize(520,300);
+        this.frame.setSize(520, 300);
         //f.getContentPane().setBackground(Color.WHITE);
         JLabel title, instr, rec;
-        title = new JLabel("ONLINE MODE!",JLabel.CENTER);
-        instr = new JLabel("In online mode, the BOT will play Cards Against Humanity on a Website",JLabel.CENTER);
+        title = new JLabel("ONLINE MODE!", JLabel.CENTER);
+        instr = new JLabel("In online mode, the BOT will play Cards Against Humanity on a Website", JLabel.CENTER);
         title.setFont(new Font("Serif", Font.BOLD, 24));
         instr.setFont(new Font("Serif", Font.PLAIN, 14));
 
-        title.setBounds(0,20, 520,20);
+        title.setBounds(0, 20, 520, 20);
         //title.setSize(350,100);
-        instr.setBounds(0,100, 520,20);
+        instr.setBounds(0, 100, 520, 20);
 
-        JButton b=new JButton("Launch Online Mode");
-        b.setBounds(150,200,200,30);
+        this.b = new JButton("Launch Online Mode");
+        this.b.setBounds(150, 200, 200, 30);
 
-        b.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                offlineMode om = new offlineMode();
+        this.b.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                set_line(true);
                 //f.dispose();
             }
         });
 
 
-        frame.add(b);
+
+
+        this.frame.add(b);
 
         //f.add(br);
         //f.add(rec);
-        frame.add(title);
-        frame.add(instr);
-        frame.setLayout(null);
-        frame.setVisible(true);
+        this.frame.add(title);
+        this.frame.add(instr);
+        this.frame.setLayout(null);
+        this.frame.setVisible(true);
 
 
+    }
+
+    public void set_line(boolean online){
+        if(online){
+            //frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            this.b.setText("Exit Online Mode");
+
+            this.b.removeActionListener(this.b.getActionListeners()[0]);
+
+            this.b.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    set_line(false);
+                    //f.dispose();
+                }
+            });
+        }else {
+            this.b.setText("Launch Online Mode");
+
+            this.b.removeActionListener(this.b.getActionListeners()[0]);
+
+            this.b.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    set_line(true);
+                    //f.dispose();
+                }
+            });
+        }
     }
 
     public static void main(String[]args)
